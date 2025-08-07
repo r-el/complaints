90ae57313786a1063ced0bf9c34284492d91cc6b
Ariel Tanami <arieltanami122@gmail.com>
2025-08-07 12:48:20 +0300

    Remove basic route handler from app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const HOST = process.env.HOST || "localhost";
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"))); // May need to move this line after the routes
 
 // Log middleware
 app.use((req, res, next) => {
@@ -17,11 +17,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// Basic route
-app.get("/", (req, res) => {
-  // res.sendFile(path.join(__dirname, "public", "index.html"));
-  res.json({ message: "Welcome to the Complaints Project API" });
-});
 
 // Start server
 app.listen(PORT, () => {
