1bd4a96a925366e14e1a198be49c2556e2aacd00
Ariel Tanami <arieltanami122@gmail.com>
2025-08-12 09:51:54 +0300

    Refactor complaint controller: reorder and clean up getComplaintForm function

--- controllers/complaintsController.js
+++ controllers/complaintsController.js
@@ -1,28 +1,31 @@
 const Complaint = require('../models/complaint.model');
 const path = require('path');
 
+// Get complaint form page
+exports.getComplaintForm = (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/complaint.html'));
+};
+
 // Submit a new complaint
 exports.submitComplaint = async (req, res) => {
   try {
     const { category, message } = req.body;
     
+    // Validate input
     if (!category || !message) {
       return res.status(400).send('Please provide category and message');
     }
     
+    // Create new complaint
     await Complaint.create({
       category,
       message
     });
     
+    console.log('New complaint submitted:', { category, message });
     res.redirect('/'); // Redirect back to home page after submission
   } catch (error) {
     console.error('Error submitting complaint:', error);
     res.status(500).send('Server error');
   }
-};
-
-// Get complaints submission form
-exports.getComplaintForm = (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/complaint.html'));
 };
